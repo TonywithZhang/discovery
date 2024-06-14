395ee8d08b484114486270e2af48b1e28e1c89b0
zhangqingde <18762675195@126.com>
2024-06-14 18:46:32 +0800

    修改：microbit官方rust例子

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,31 @@
 #![no_std]
 
 use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::display::blocking::Display;
+use microbit::hal::prelude::*;
+use microbit::hal::Timer;
 use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let heart = [
+        [0, 1, 0, 1, 0],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+    ];
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        // Show heart for 1000ms
+        display.show(&mut timer, heart, 1000);
+        display.clear();
+        timer.delay_ms(1000_u32);
+    }
 }
